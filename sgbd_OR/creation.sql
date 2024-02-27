7bc6ddee3308982527b03f1d936aea9d5d642c6a
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2024-02-27 14:20:37 -0300

    Update creation.sql

--- sgbd_OR/creation.sql
+++ sgbd_OR/creation.sql
@@ -15,6 +15,13 @@ CREATE OR REPLACE TYPE endereco_tp AS OBJECT(
     numero_residencia NUMBER
 );
 /
+-- Telefone
+CREATE OR REPLACE TYPE varray_telefone AS VARRAY(2) OF telefone_tp;
+/
+CREATE OR REPLACE TYPE telefone_tp AS OBJECT(
+    	numero VARCHAR2(50)
+);
+/
 
 -- Pessoa
 CREATE OR REPLACE TYPE pessoa_tp AS OBJECT(
@@ -22,7 +29,8 @@ CREATE OR REPLACE TYPE pessoa_tp AS OBJECT(
 	nome VARCHAR2(50),
     data_de_nascimento DATE,
     genero VARCHAR2(50),
-    cep VARCHAR2(50),
+    cep REF endereco_tp,
+    telefones varray_telefone,
 
     MEMBER PROCEDURE get_pessoa_endereco (SELF pessoa_tp),
     FINAL MEMBER PROCEDURE get_pessoa_info (SELF pessoa_tp)
@@ -44,13 +52,6 @@ END;
 
 /
 --
-
--- Telefone
-CREATE OR REPLACE TYPE telefone_tp AS OBJECT(
-        cpf VARCHAR2(50),
-    	numero VARCHAR2(50)
-);
-/
 
 -- Funcionário
 CREATE OR REPLACE TYPE funcionario_tp UNDER pessoa_tp(
@@ -90,7 +91,7 @@ CREATE OR REPLACE TYPE detalhesRaca_tp AS OBJECT(
         especie VARCHAR2
     ) RETURN SELF AS RESULT
     
-);
+)NOT INSTANTIABLE;
 /
 CREATE OR REPLACE TYPE BODY detalhesRaca_tp AS
     CONSTRUCTOR FUNCTION detalhesRaca_tp(
@@ -107,18 +108,18 @@ END;
 
 /
 CREATE OR REPLACE TYPE pet_tp AS OBJECT(
-    cpf_Responsavel VARCHAR2(50),
+    cpf_Responsavel REF pessoa_tp,
     nome VARCHAR2(50),
-    raca VARCHAR2(50),
+    raca REF detalhesRaca_tp,
     data_nascimento DATE,
     genero VARCHAR2(50),
     observacoes VARCHAR2(50)
 );
 /
 CREATE OR REPLACE TYPE atendimento_tp AS OBJECT(
-    cpf_Cliente VARCHAR2(50),
+    cpf_Cliente REF cliente_tp,
     data_atendimento DATE,
-    cpf_Funcionario VARCHAR2(50),
+    cpf_Funcionario REF funcionario_tp,
     custo_total NUMBER,
     metodo_pagamento VARCHAR2(50)
 );
@@ -129,19 +130,24 @@ CREATE OR REPLACE TYPE servico_tp AS OBJECT(
     descricao VARCHAR2(50),
     custo NUMBER,
 
-    cpf_Cliente VARCHAR2(50),
+    cpf_Cliente REF cliente_tp,
     data_atendimento DATE
 );
+/
+CREATE TYPE caracteristicas_list AS TABLE OF VARCHAR2(200);
 /
 CREATE OR REPLACE TYPE produto_tp AS OBJECT(
     id VARCHAR2(50),
     nome VARCHAR2(50),
     preco NUMBER,
     quantidade NUMBER,
-    categoria VARCHAR2(50),
     marca VARCHAR2(50),
-    caracteristicas VARCHAR2(50)
+    caracteristicas caracteristicas_list
 );
+/
+    
+ALTER TYPE produto_tp ADD ATTRIBUTE(categoria VARCHAR2(50)) CASCADE;
+
 /
 CREATE OR REPLACE TYPE equipamento_tp AS OBJECT(
     id VARCHAR2(50),
@@ -178,5 +184,97 @@ CREATE OR REPLACE TYPE vende_tp AS OBJECT(
     data_atendimento DATE,
     id_Produto VARCHAR2(50)
 );
+/
+-- Criação de instâncias das tabelas
+
+CREATE TABLE Endereco of endereco_tp(
+    cep PRIMARY KEY,
+    rua NOT NULL,
+    bairro NOT NULL,
+    cidade NOT NULL,
+    numero_residencia NOT NULL
+);
+/
+--
+CREATE TABLE Telefone of telefone_tp(
+    numero PRIMARY KEY
+);
+/
+--
+CREATE TABLE Pessoa of pessoa_tp(
+    cpf PRIMARY KEY,
+	nome NOT NULL,
+    data_de_nascimento NOT NULL,
+    genero NOT NULL,
+    cep WITH ROWID REFERENCES Endereco
+);
+/
+--
+CREATE TABLE Funcionario of funcionario_tp(
+    cargo NOT NULL,
+    salario NOT NULL,
+    email NOT NULL,
+    senha NOT NULL,
+    data_contratacao NOT NULL,
+    supervisor SCOPE IS funcionario_tp,
+
+    CONSTRAINT PRIMARY KEY Funcionario_PK (cpf)
+);
+/
+--
+CREATE TABLE Cliente of cliente_tp(
+    data_primeiro_atendimento NOT NULL,
+    creditos NOT NULL,
+    CONSTRAINT PRIMARY KEY Funcionario_PK (cpf)
+);
+/
+--
+CREATE TABLE DetalhesRaca of detalhesRaca_tp(
+    raca PRIMARY KEY,
+    especie NOT NULL
+);
+/
+--
+CREATE TABLE Pet of pet_tp(
+    cpf_Responsavel WITH ROWID REFERENCES Pessoa,
+    nome NOT NULL,
+    raca WITH ROWID REFERENCES DetalhesRaca,
+    data_nascimento NOT NULL,
+    genero NOT NULL,
+    observacoes NOT NULL
+);
+/
+--
+CREATE TABLE Atendimento of atendimento_tp(
+    cpf_Cliente WITH ROWID REFERENCES Cliente,
+    data_atendimento NOT NULL,
+    cpf_Funcionario WITH ROWID REFERENCES Funcionario,
+    custo_total NOT NULL,
+    metodo_pagamento NOT NULL
+);
+/
+--
+CREATE TABLE Servico OF servico_tp(
+    codigo NOT NULL,
+    nome NOT NULL,
+    descricao NOT NULL,
+    custo NOT NULL,
+
+    cpf_Cliente WITH ROWID REFERENCES Cliente,
+    data_atendimento NOT NULL
+);
+/
+--
+
+CREATE TABLE Produto of produto_tp(
+    id NOT NULL,
+    nome NOT NULL,
+    preco NOT NULL,
+    quantidade NOT NULL,
+    marca NOT NULL,
+    categoria NOT NULL,
+) NESTED TABLE caracteristicas STORE AS NT_CaracteristicasProduto;
+
+
 
  
