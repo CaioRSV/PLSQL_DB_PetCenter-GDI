747e29e5c2cf099b2c6c6f921d6d1e2be9f2869f
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2024-03-04 17:12:14 -0300

    Update queries.sql

--- sgbd_OR/queries.sql
+++ sgbd_OR/queries.sql
@@ -1,7 +1,7 @@
--- Consulta com Update com REF
+--- Consulta com Update com REF
 UPDATE Pet p SET p.cpf_Responsavel = (SELECT REF(R) FROM Pessoa R WHERE R.cpf='123456789-06') WHERE p.nome='Destruidor';
 
--- Consulta a PET e Pessoa, com DEREF
+--- Consulta a PET e Pessoa, com DEREF
 SELECT p.nome, DEREF(p.cpf_Responsavel).nome AS Novo_Responsavel FROM Pet p WHERE p.nome='Destruidor';
 
 -- Consulta de Endereco
@@ -14,7 +14,7 @@ SELECT numero FROM Telefone ORDER BY numero;
 -- Selecionando informações sobre pessoas e seus números de telefone / Consulta a VARRAY
 SELECT p.cpf, p.nome, p.data_de_nascimento, p.genero, t.numero FROM Pessoa p CROSS JOIN TABLE(p.telefones) t;
 
--- Consulta a VARRAY
+--- Consulta a VARRAY
 SELECT t.numero FROM Pessoa p CROSS JOIN TABLE(p.telefones) t;
 
 --Funcionário e seu supervisor
@@ -26,6 +26,5 @@ SELECT c.nome, c.endereco.cidade FROM Cliente c;
 -- Pet (Selecionando nome dos animais e seus donos)
 SELECT p.nome, DEREF(p.cpf_Responsavel).nome FROM Pet p;
 
-
 --- Consulta a NESTED TABLE / Consulta a Características
-SELECT c.column_value AS Caracteristicas FROM Produto p CROSS JOIN TABLE(p.carateristicas) c;
+SELECT c.column_value AS caracteristicas FROM Produto p CROSS JOIN TABLE(p.caracteristicas) c;
